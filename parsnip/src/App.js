7b6b1c87252cc2897590b3eaf86bc7503c54f67f
mtraina <matteo.traina.mail@gmail.com>
2018-11-24 20:54:24 +1300

    tasks created

--- parsnip/src/App.js
+++ parsnip/src/App.js
@@ -1,25 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import TasksPage from './components/TasksPage';
+
+const mockTasks = [
+  {
+    id: 1,
+    title: 'Learn Redux',
+    description: 'The store, actions, and reducers, oh my!', status: 'In Progress',
+  },
+  {
+    id: 2,
+    title: 'Peace on Earth', description: 'No big deal.', status: 'In Progress',
+  }
+];
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="main­content">
+        <TasksPage tasks={mockTasks} />
       </div>
     );
   }
--- parsnip/src/index.js
+++ parsnip/src/index.js
@@ -1,12 +1,20 @@
+import registerServiceWorker from './registerServiceWorker';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import tasks from './reducers';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(tasks);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
+
+registerServiceWorker();
