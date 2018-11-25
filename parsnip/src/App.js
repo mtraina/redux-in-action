6702fd09c6afb7db1941368ab6185a539f630143
mtraina <matteo.traina.mail@gmail.com>
2018-11-25 19:55:54 +1300

    source code aligned with book

--- parsnip/src/App.js
+++ parsnip/src/App.js
@@ -1,26 +1,34 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import TasksPage from './components/TasksPage';
-
-const mockTasks = [
-  {
-    id: 1,
-    title: 'Learn Redux',
-    description: 'The store, actions, and reducers, oh my!', status: 'In Progress',
-  },
-  {
-    id: 2,
-    title: 'Peace on Earth', description: 'No big deal.', status: 'In Progress',
-  }
-];
+import { createTask, editTask } from './actions';
 
 class App extends Component {
+  onCreateTask = ({ title, description }) => {
+    this.props.dispatch(createTask({ title, description }));
+  };
+
+  onStatusChange = (id, status) => {
+    this.props.dispatch(editTask(id, { status }));
+  };
+
   render() {
     return (
-      <div className="main­content">
-        <TasksPage tasks={mockTasks} />
+      <div className="main-content">
+        <TasksPage
+          tasks={this.props.tasks}
+          onCreateTask={this.onCreateTask}
+          onStatusChange={this.onStatusChange}
+        />
       </div>
     );
   }
 }
 
-export default App;
+function mapStateToProps(state) {
+  return {
+    tasks: state.tasks,
+  };
+}
+
+export default connect(mapStateToProps)(App);
